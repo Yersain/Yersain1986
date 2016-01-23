dfeafc159284e41bcfdcb8ff5541b10613ac2ccf
Yersain <yersa.kz@gmail.com>
2016-01-24 04:45:34 +0600

    Initial create. Added Admin area

---
+++ lab/Models/Disease.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lab.Models
+{
+    public class Disease
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public DiseaseGroup DiseaseGroup { get; set; }
+        public int DiseaseGroupId { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ lab/Models/DiseaseGroup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lab.Models
+{
+    public class DiseaseGroup
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Disease> Diseases { get; set; }
+        public ICollection<DiseaseGroup> DiseaseGroups { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
